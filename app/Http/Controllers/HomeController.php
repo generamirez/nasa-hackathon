d4af99d898a5e1011a39f345bf5045d06e7d5eec
Gene <generamirez@xolveit.com>
2021-02-21 10:53:01 +0800

    visitor

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App;
+use App, DB;
 use App\ForeCast;
 use Carbon\Carbon;
 use App\Plant;
 use App\Http\Requests\PlantShowRequest;
+use Browser;
+use App\Visitor;
 
 class HomeController extends Controller
 {
@@ -68,17 +70,30 @@ class HomeController extends Controller
 
     public function test (Request $request)
     {
+        DB::beginTransaction();
+        $ipText = '';
         foreach (array('HTTP_CLIENT_IP', 'HTTP_X_FORWARDED_FOR', 'HTTP_X_FORWARDED', 'HTTP_X_CLUSTER_CLIENT_IP', 'HTTP_FORWARDED_FOR', 'HTTP_FORWARDED', 'REMOTE_ADDR') as $key){
             if (array_key_exists($key, $_SERVER) === true){
                 foreach (explode(',', $_SERVER[$key]) as $ip){
                     $ip = trim($ip); // just to be safe
                     if (filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_NO_PRIV_RANGE | FILTER_FLAG_NO_RES_RANGE) !== false){
-                        return $ip;
+                        $ipText = $ip;
                     }
                 }
             }
         }
-        return request()->ip(); // it will return server ip when no client ip found
+        $visitor  = new Visitor;
+        $visitor->user_agent = Browser::userAgent();
+        $visitor->platform = Browser::platformName();
+        $visitor->platform_version = Browser::platformVersion();
+        $visitor->platform_family = Browser::platformFamily();
+        $visitor->device_model = Browser::deviceModel();
+        $visitor->ip_address = $ipText;
+        $visitor->save();
+
+        // dd($visitor);
+        DB::commit();
+        // return request()->ip(); // it will return server ip when no client ip found
         // dd($request->ip());
     }
 }
